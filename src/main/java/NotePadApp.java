92d12f3f4327dd4d233c1c354b41eca7c85477ce
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2022-02-02 13:24:57 +0300

    start project work

--- src/main/java/NotePadApp.java
+++ src/main/java/NotePadApp.java
@@ -1,5 +1,10 @@
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class NotePadApp extends Application {
     public static void main(String[] args) {
@@ -8,6 +13,17 @@ public class NotePadApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("NotePad");
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(700);
+
+        try (InputStream iconStream = getClass().getResourceAsStream("/icon.png")) {
+            Image image = new Image(iconStream);
+            primaryStage.getIcons().add(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         primaryStage.show();
     }
 }
