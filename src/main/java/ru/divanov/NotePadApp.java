c9c66dcec9119de2a5d6b211e2c07cc6edbbf550
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2022-02-03 16:22:13 +0300

    mod main class

--- src/main/java/ru/divanov/NotePadApp.java
+++ src/main/java/ru/divanov/NotePadApp.java
@@ -1,15 +1,30 @@
 package ru.divanov;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class NotePadApp extends Application {
-    public static void main(String[] args) {
+    private final String TITLE = "NotePad";
 
+    public static void main(String[] args) {
+        launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/NotePad.fxml"));
+            BorderPane rootLayOut = loader.load();
+            primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/icon.png")));
+            primaryStage.setTitle(TITLE);
+            primaryStage.setScene(new Scene(rootLayOut));
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
