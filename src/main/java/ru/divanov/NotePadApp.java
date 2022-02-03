e14a708566f342523ea415b9830945f1dc5e57b6
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2022-02-03 15:35:58 +0300

    import java fx

--- src/main/java/ru/divanov/NotePadApp.java
+++ src/main/java/ru/divanov/NotePadApp.java
@@ -1,7 +1,15 @@
 package ru.divanov;
 
-public class NotePadApp {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class NotePadApp extends Application {
     public static void main(String[] args) {
 
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
     }
 }
